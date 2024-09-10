3dd58ebccbd873d3ef40887c98cbd8bdaf5f82ba
Ronnie Berman <ronnie@envilope.io>
2024-09-10 22:35:38 +0300

    0.6.9

--- Casks/envilope.rb
+++ Casks/envilope.rb
@@ -1,8 +1,8 @@
 cask "envilope" do
   version "0.6.5"
-  sha256 "ffe359806cf4abd397539f77d1cbe8829adb31ee1446f675da64f909c6180aba"
+  sha256 "f27cfb4fc6646ecfaeb86fd245843e0bd19411f727b32def0bd409bb69245b64"
 
-  url "https://78PAcXfL3lMougPqAWCOqZ14eleEopQj4cERTfC5AmXcMe8mhTQX7bqYoUgCg7P.envilope.io/0.6.5/darwin/mac_installer.sh"
+  url "https://78PAcXfL3lMougPqAWCOqZ14eleEopQj4cERTfC5AmXcMe8mhTQX7bqYoUgCg7P.envilope.io/0.6.9/darwin/mac_installer.sh"
   name "envilope.io"
   desc "The fast, simple, and secure solution to k8s development."
   homepage "https://envilope.io"
